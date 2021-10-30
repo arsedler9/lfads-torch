6644776daf60214123858faae8074cd5105748b4
Andrew Sedler <arsedler9@gmail.com>
2021-10-30 14:29:34 -0400

    Update encoder

--- lfads_torch/modules/encoder.py
+++ lfads_torch/modules/encoder.py
@@ -50,24 +50,40 @@ class Encoder(nn.Module):
 
     def forward(self, data: torch.Tensor):
         hps = self.hparams
-
-        # Pass data through IC encoder
+        assert data.shape[1] == hps.seq_len, (
+            f"Sequence length specified in HPs ({hps.seq_len}) "
+            "must match data dim 1 ({data.shape[1]})."
+        )
         data_drop = self.dropout(data)
-        _, h_n = self.ic_enc(data_drop, self.ic_enc_h0)
+        # option to use separate segment for IC encoding
+        if hps.ic_enc_seq_len > 0:
+            ic_enc_data = data_drop[:, : hps.ic_enc_seq_len, :]
+            ci_enc_data = data_drop[:, hps.ic_enc_seq_len :, :]
+        else:
+            ic_enc_data = data_drop
+            ci_enc_data = data_drop
+        # Pass data through IC encoder
+        _, h_n = self.ic_enc(ic_enc_data, self.ic_enc_h0)
         # Combine output from fwd and bwd encoders
         h_n = torch.cat([*h_n], -1)
         # Compute initial condition posterior
         h_n_drop = self.dropout(h_n)
         ic_params = self.ic_linear(h_n_drop)
         ic_mean, ic_logstd = torch.split(ic_params, hps.ic_dim, dim=1)
-        ic_post = Independent(Normal(ic_mean, torch.exp(ic_logstd)))
-        # Pass data through CI encoder
-        ci, _ = self.ci_enc(data_drop, self.ci_enc_h0)
-        # Add a lag to the controller input
-        ci_fwd, ci_bwd = torch.split(ci, hps.ci_enc_dim, axis=2)
-        ci_fwd = nn.functional.pad(ci_fwd, (0, 0, hps.ci_lag, 0, 0, 0))
-        ci_bwd = nn.functional.pad(ci_bwd, (0, 0, 0, hps.ci_lag, 0, 0))
-        ci = torch.cat([ci_fwd[:, : hps.seq_len, :], ci_bwd[:, -hps.seq_len :, :]])
+        ic_std = torch.exp(ic_logstd) + hps.ic_post_var_min
+        ic_post = Independent(Normal(ic_mean, ic_std))
+        if hps.ci_enc_dim > 0:
+            # Pass data through CI encoder
+            ci, _ = self.ci_enc(ci_enc_data, self.ci_enc_h0)
+            # Add a lag to the controller input
+            ci_fwd, ci_bwd = torch.split(ci, hps.ci_enc_dim, axis=2)
+            ci_fwd = nn.functional.pad(ci_fwd, (0, 0, hps.ci_lag, 0, 0, 0))
+            ci_bwd = nn.functional.pad(ci_bwd, (0, 0, 0, hps.ci_lag, 0, 0))
+            ci_len = hps.seq_len - hps.ic_enc_seq_len
+            ci = torch.cat([ci_fwd[:, :ci_len, :], ci_bwd[:, -ci_len:, :]])
+        else:
+            ci = torch.zeros_like(ci_enc_data)[:, :, : 2 * hps.ci_enc_dim]
+
         return ic_post, ci
 
     def update_hparams(self, hparams: dict):
