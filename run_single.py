27fb8c9493f7469fc3533320c2b1f5d7f5ed7890
Andrew Sedler <arsedler9@gmail.com>
2021-11-06 21:04:26 -0400

    Add config and run script for multi-runs

--- run_single.py
+++ run_single.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig
 # dotenv.load_dotenv(override=True)
 
 
-@hydra.main(config_path="configs/", config_name="config.yaml")
+@hydra.main(config_path="configs/", config_name="config_single.yaml")
 def main(config: DictConfig):
 
     # Imports should be nested inside @hydra.main to optimize tab completion
