5793aa3be65120190b55a1048a4021d696f3ef14
Andrew Sedler <arsedler9@gmail.com>
2022-02-28 19:03:32 -0500

    Add autoregressive CO prior

--- lfads_torch/models/modules/priors.py
+++ lfads_torch/models/modules/priors.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.distributions import Independent, Normal, kl_divergence
+from torch.distributions.transforms import AffineTransform
 
 
 class MultivariateNormal(nn.Module):
@@ -9,15 +10,13 @@ class MultivariateNormal(nn.Module):
         mean,
         variance,
         shape,
-        trainable_means=True,
-        trainable_vars=False,
     ):
         super().__init__()
         # Create distribution parameter tensors
         means = torch.ones(shape) * mean
         logvars = torch.log(torch.ones(shape) * variance)
-        self.mean = nn.Parameter(means, requires_grad=trainable_means)
-        self.logvar = nn.Parameter(logvars, requires_grad=trainable_vars)
+        self.mean = nn.Parameter(means, requires_grad=True)
+        self.logvar = nn.Parameter(logvars, requires_grad=False)
 
     def forward(self, post_mean, post_std):
         # Create the posterior distribution
@@ -27,4 +26,44 @@ class MultivariateNormal(nn.Module):
         prior = Independent(Normal(self.mean, prior_std), 1)
         # Compute KL analytically
         kl_batch = kl_divergence(posterior, prior)
+        return torch.mean(kl_batch)
+
+
+class AutoregressiveMultivariateNormal(nn.Module):
+    def __init__(
+        self,
+        tau,
+        nvar,
+        shape,
+    ):
+        super().__init__()
+        # Create the distribution parameters
+        logtaus = torch.log(torch.ones(shape) * tau)
+        lognvars = torch.log(torch.ones(shape) * nvar)
+        self.logtaus = nn.Parameter(logtaus, requires_grad=True)
+        self.lognvars = nn.Parameter(lognvars, requires_grad=True)
+
+    def log_prob(self, sample):
+        # Compute alpha and process variance
+        alphas = torch.exp(-1.0 / torch.exp(self.logtaus))
+        logpvars = self.lognvars - torch.log(1 - alphas ** 2)
+        # Create autocorrelative transformation
+        transform = AffineTransform(loc=0, scale=alphas)
+        # Align previous samples and compute means and stddevs
+        prev_samp = torch.roll(sample, shifts=1, dims=1)
+        means = transform(prev_samp)
+        stddevs = torch.ones_like(means) * torch.exp(0.5 * self.lognvars)
+        # Correct the first time point
+        means[:, 0] = 0.0
+        stddevs[:, 0] = torch.exp(0.5 * logpvars)
+        # Create the prior and compute the log-probability
+        prior = Independent(Normal(means, stddevs), 1)
+        return prior.log_prob(sample)
+
+    def forward(self, post_mean, post_std):
+        posterior = Independent(Normal(post_mean, post_std), 1)
+        sample = posterior.rsample()
+        log_q = posterior.log_prob(sample)
+        log_p = self.log_prob(sample)
+        kl_batch = log_q - log_p
         return torch.mean(kl_batch)
