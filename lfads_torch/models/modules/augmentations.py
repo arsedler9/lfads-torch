09cdcdd5a12337084f7f3713fc7d71d0d005c816
Andrew Sedler <arsedler9@gmail.com>
2022-06-02 21:21:15 -0400

    Add deEMG data and model; minor deEMG changes

--- lfads_torch/models/modules/augmentations.py
+++ lfads_torch/models/modules/augmentations.py
@@ -35,11 +35,8 @@ class AugmentationStack:
 
 
 class TemporalShift(nn.Module):
-    def __init__(self, seed=None, std=3.0, max_shift=6):
+    def __init__(self, std=3.0, max_shift=6):
         super().__init__()
-        self.rng = torch.Generator()
-        if seed is not None:
-            self.rng.manual_seed(seed)
         self.std = std
         self.max = max_shift
 
@@ -54,15 +51,14 @@ class TemporalShift(nn.Module):
         batch_size, n_steps, n_channels = data.shape
         # Sample a shift for each channel and each sample
         mean = torch.zeros(batch_size, 1, n_channels)
-        shifts = torch.normal(mean, self.std, generator=self.rng)
-        shifts = shifts.to(data.device)
+        shifts = torch.normal(mean, self.std).to(data.device)
         # Clamp the shifts within the range
         shifts = torch.clamp(shifts.round().long(), -self.max, self.max)
         # Create indices and shift them based on the sampled values
         indices = torch.arange(n_steps, device=data.device)[None, :, None]
         indices = indices.repeat(batch_size, 1, n_channels)
-        # TODO: Fix rollover bug (zeros?)
-        shifted_indices = (indices - shifts) % n_steps
+        # Repeat end values on the edges
+        shifted_indices = torch.clamp(indices - shifts, 0, n_steps - 1)
         # Apply the shifts to the data
         shifted_data = torch.gather(data, dim=1, index=shifted_indices)
         return shifted_data
