82e18e05b635f7a02f170d91da68c4dec2c6d9af
Andrew Sedler <arsedler9@gmail.com>
2022-06-29 15:26:46 -0400

    Update to correctly initialize linear layers

--- lfads_torch/models/modules/initializers.py
+++ lfads_torch/models/modules/initializers.py
@@ -7,6 +7,12 @@ def init_variance_scaling_(weight, scale_dim: int):
     nn.init.normal_(weight, std=1 / torch.sqrt(scale_dim))
 
 
+def init_linear_(linear: nn.Linear):
+    init_variance_scaling_(linear.weight, linear.in_features)
+    if linear.bias is not None:
+        nn.init.zeros_(linear.bias)
+
+
 def init_gru_cell_(cell: nn.GRUCell, scale_dim: int = None):
     if scale_dim is None:
         ih_scale = cell.input_size
@@ -15,7 +21,6 @@ def init_gru_cell_(cell: nn.GRUCell, scale_dim: int = None):
         ih_scale = hh_scale = scale_dim
     init_variance_scaling_(cell.weight_ih, ih_scale)
     init_variance_scaling_(cell.weight_hh, hh_scale)
-    # TODO: confirm that the gate weights come before candidate
     nn.init.ones_(cell.bias_ih)
     cell.bias_ih.data[-cell.hidden_size :] = 0.0
     # NOTE: these weights are not present in TF
