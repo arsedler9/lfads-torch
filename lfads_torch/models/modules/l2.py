a275ebed30db332aa14cfa3af31aabf45705ee14
Andrew Sedler <arsedler9@gmail.com>
2023-03-10 11:11:21 -0500

    Simplify recurrent size calculation

--- lfads_torch/models/modules/l2.py
+++ lfads_torch/models/modules/l2.py
@@ -1,22 +1,6 @@
 import torch
 
 
-def compute_recurrent_size(hps):
-    recurrent_units_and_weights = [
-        (hps.ic_enc_dim, hps.l2_ic_enc_scale),
-        (hps.ic_enc_dim, hps.l2_ic_enc_scale),
-        (hps.ci_enc_dim, hps.l2_ci_enc_scale),
-        (hps.ci_enc_dim, hps.l2_ci_enc_scale),
-        (hps.gen_dim, hps.l2_gen_scale),
-        (hps.con_dim, hps.l2_con_scale),
-    ]
-    model_recurrent_size = 0
-    for units, weight in recurrent_units_and_weights:
-        if weight > 0:
-            model_recurrent_size += 3 * units ** 2
-    return model_recurrent_size
-
-
 def compute_l2_penalty(lfads, hps):
     recurrent_kernels_and_weights = [
         (lfads.encoder.ic_enc.fwd_gru.cell.weight_hh, hps.l2_ic_enc_scale),
@@ -33,9 +17,11 @@ def compute_l2_penalty(lfads, hps):
         )
     # Add recurrent penalty
     recurrent_penalty = 0.0
+    recurrent_size = 0
     for kernel, weight in recurrent_kernels_and_weights:
-        recurrent_penalty += weight * 0.5 * torch.norm(kernel, 2) ** 2
-    recurrent_size = compute_recurrent_size(hps)
+        if weight > 0:
+            recurrent_penalty += weight * 0.5 * torch.norm(kernel, 2) ** 2
+            recurrent_size += kernel.numel()
     recurrent_penalty /= recurrent_size + 1e-8
     # Add recon penalty if applicable
     recon_penalty = 0.0
