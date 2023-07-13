ce2189e1eb5add10c38da7e1c9000f2ba7f8db1c
Andrew Sedler <arsedler9@gmail.com>
2023-07-13 14:42:38 -0400

    Add step-wise evaluation of regional BPS

--- lfads_torch/metrics.py
+++ lfads_torch/metrics.py
@@ -1,4 +1,7 @@
+import math
+
 import torch
+from torch.nn.functional import poisson_nll_loss
 from torchmetrics import Metric
 
 
@@ -11,6 +14,34 @@ def r2_score(preds, targets):
     ss_tot = torch.sum((targets - target_mean) ** 2, dim=0)
     ss_res = torch.sum((targets - preds) ** 2, dim=0)
     return torch.mean(1 - ss_res / ss_tot)
+
+
+def bits_per_spike(preds, targets):
+    """
+    Computes BPS for n_samples x n_timesteps x n_neurons arrays.
+    Preds are logrates and targets are binned spike counts.
+    """
+    nll_model = poisson_nll_loss(preds, targets, full=True, reduction="sum")
+    nll_null = poisson_nll_loss(
+        torch.mean(targets, dim=(0, 1), keepdim=True),
+        targets,
+        log_input=False,
+        full=True,
+        reduction="sum",
+    )
+    return (nll_null - nll_model) / torch.nansum(targets) / math.log(2)
+
+
+def regional_bits_per_spike(preds, targets, encod_data_dim, encod_seq_len):
+    """
+    Computes BPS for n_samples x n_timesteps x n_neurons arrays, separated into
+    heldin and heldout regions.
+    """
+    edd, esl = encod_data_dim, encod_seq_len
+    bps = bits_per_spike(preds[:, :esl, :edd], targets[:, :esl, :edd])
+    co_bps = bits_per_spike(preds[:, :esl, edd:], targets[:, :esl, edd:])
+    fp_bps = bits_per_spike(preds[:, esl:], targets[:, esl:])
+    return bps, co_bps, fp_bps
 
 
 class ExpSmoothedMetric(Metric):
