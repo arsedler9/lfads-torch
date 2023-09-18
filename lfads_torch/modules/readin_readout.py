5e643898b540affd10761fcee3931149f292a20a
Andrew Sedler <arsedler9@gmail.com>
2023-09-18 15:30:03 -0400

    Add `HDF5InitLinear` for PCR-based readin init

--- lfads_torch/modules/readin_readout.py
+++ lfads_torch/modules/readin_readout.py
@@ -1,9 +1,7 @@
 import math
 
-# import torch
+import h5py
 from torch import nn
-
-# import h5py
 
 
 class FanInLinear(nn.Linear):
@@ -13,14 +11,12 @@ class FanInLinear(nn.Linear):
         nn.init.constant_(self.bias, 0.0)
 
 
-# class PCRInitModuleList(nn.ModuleList):
-#     def __init__(self, inits_path: str, modules: list[nn.Module]):
-#         super().__init__(modules)
-#         # Pull pre-computed initialization from the file, assuming correct order
-#         with h5py.File(inits_path, "r") as h5file:
-#             weights = [v["/" + k + "/matrix"][()] for k, v in h5file.items()]
-#             biases = [v["/" + k + "/bias"][()] for k, v in h5file.items()]
-#         # Load the state dict for each layer
-#         for layer, weight, bias in zip(self, weights, biases):
-#             state_dict = {"weight": torch.tensor(weight), "bias": torch.tensor(bias)}
-#             layer.load_state_dict(state_dict)
+class HDF5InitLinear(nn.Linear):
+    def __init__(self, inits_path: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with h5py.File(inits_path) as h5file:
+            state_dict = {
+                "weight": h5file["readin_init_weight"][()],
+                "bias": h5file["readin_init_bias"][()],
+            }
+        self.load_state_dict(state_dict)
