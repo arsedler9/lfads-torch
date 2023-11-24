f1dea963022ad9b2ac5eb8897d91895e28527369
Brianna <brianna.karpowicz@gmail.com>
2023-11-24 10:04:55 -0500

    Add layers, config changes, and tutorial for multisession PCR initialization (#5)
    This PR adds functionality for PCR-based initialization of readin and readout layers for multisession modeling. All changes are enumerated below:
    
    * We added notebooks, scripts, and configutation files, and data download links to demonstrate running the multisession modeling pipeline, from data preprocessing to modeling to evaluation. 
    
    * The `data_paths` argument of the `BasicDataModule` has been replaced by a `datafile_pattern` argument which can use the `glob` wildcard. This eliminates the need to manually specify names of all input files. The example config files have been updated accordingly.
    
    * We added new `MultisessionReadin` and `MultisessionReadout` layers that automatically infer the number of sessions and channel counts for each session. These layers also allow the user to specify whether to use the PCR initialization and set trainability.
    
    * We added a new `MultisessionReconstruction` layer, which automatically creates a reconstruction object for each session.
    
    * We included a missing `scikit-learn` dependency in `requirements.txt`.
    
    ---------
    
    Co-authored-by: Andrew Sedler <arsedler9@gmail.com>
    Co-authored-by: anjaliagarwal8 <anjaliagrl8@gmail.com>

--- lfads_torch/modules/readin_readout.py
+++ lfads_torch/modules/readin_readout.py
@@ -1,9 +1,11 @@
+import abc
 import math
+from glob import glob
 
-# import torch
+import h5py
+import numpy as np
+import torch
 from torch import nn
-
-# import h5py
 
 
 class FanInLinear(nn.Linear):
@@ -13,14 +15,98 @@ class FanInLinear(nn.Linear):
         nn.init.constant_(self.bias, 0.0)
 
 
-# class PCRInitModuleList(nn.ModuleList):
-#     def __init__(self, inits_path: str, modules: list[nn.Module]):
-#         super().__init__(modules)
-#         # Pull pre-computed initialization from the file, assuming correct order
-#         with h5py.File(inits_path, "r") as h5file:
-#             weights = [v["/" + k + "/matrix"][()] for k, v in h5file.items()]
-#             biases = [v["/" + k + "/bias"][()] for k, v in h5file.items()]
-#         # Load the state dict for each layer
-#         for layer, weight, bias in zip(self, weights, biases):
-#             state_dict = {"weight": torch.tensor(weight), "bias": torch.tensor(bias)}
-#             layer.load_state_dict(state_dict)
+class _MultisessionModuleList(abc.ABC, nn.ModuleList):
+    def __init__(
+        self,
+        datafile_pattern: str,
+        pcr_init: bool,
+        requires_grad: bool,
+    ):
+        modules = []
+        # Identify paths that match the datafile pattern
+        data_paths = sorted(glob(datafile_pattern))
+        for data_path in data_paths:
+            if pcr_init:
+                # Load the pre-computed transformations
+                state_dict = self._get_state_dict(data_path)
+                out_features, in_features = state_dict["weight"].shape
+                layer = nn.Linear(in_features, out_features)
+                layer.load_state_dict(state_dict)
+            else:
+                # Infer only the input dimension from the file
+                in_features, out_features = self._get_layer_shape(data_path)
+                layer = nn.Linear(in_features, out_features)
+            modules.append(layer)
+        # Create the nn.ModuleList
+        super().__init__(modules)
+        # Allow the user to set requires_grad
+        for param in self.parameters():
+            param.requires_grad_(requires_grad)
+
+    @abc.abstractmethod
+    def _get_layer_shape(self, data_path):
+        pass
+
+    @abc.abstractmethod
+    def _get_state_dict(self, data_path):
+        pass
+
+
+class MultisessionReadin(_MultisessionModuleList):
+    def __init__(
+        self,
+        datafile_pattern: str,
+        out_features: int = None,
+        pcr_init: bool = True,
+        requires_grad: bool = False,
+    ):
+        assert (
+            out_features is not None
+        ) != pcr_init, "Setting `out_features` mutually excludes `pcr_init`."
+        self.out_features = out_features
+        super().__init__(
+            datafile_pattern=datafile_pattern,
+            pcr_init=pcr_init,
+            requires_grad=requires_grad,
+        )
+
+    def _get_layer_shape(self, data_path):
+        with h5py.File(data_path) as h5file:
+            in_features = h5file["train_encod_data"].shape[-1]
+        return in_features, self.out_features
+
+    def _get_state_dict(self, data_path):
+        with h5py.File(data_path) as h5file:
+            weight = h5file["readin_weight"][()]
+            bias = -np.dot(h5file["readout_bias"][()], weight)
+        return {"weight": torch.tensor(weight.T), "bias": torch.tensor(bias)}
+
+
+class MultisessionReadout(_MultisessionModuleList):
+    def __init__(
+        self,
+        datafile_pattern: str,
+        in_features: int = None,
+        pcr_init: bool = True,
+        requires_grad: bool = True,
+    ):
+        assert (
+            in_features is not None
+        ) != pcr_init, "Setting `in_features` mutually excludes `pcr_init`."
+        self.in_features = in_features
+        super().__init__(
+            datafile_pattern=datafile_pattern,
+            pcr_init=pcr_init,
+            requires_grad=requires_grad,
+        )
+
+    def _get_layer_shape(self, data_path):
+        with h5py.File(data_path) as h5file:
+            out_features = h5file["train_recon_data"].shape[-1]
+        return self.in_features, out_features
+
+    def _get_state_dict(self, data_path):
+        with h5py.File(data_path) as h5file:
+            weight = np.linalg.pinv(h5file["readin_weight"][()])
+            bias = h5file["readout_bias"][()]
+        return {"weight": torch.tensor(weight.T), "bias": torch.tensor(bias)}
